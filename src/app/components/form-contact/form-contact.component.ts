5b189f7ec79b7f5cf6cc3444f90c6e9143ae9e66
Kevin-Galarza77 <jimenezkev1040@gmail.com>
2023-11-20 23:14:53 -0500

    eliminacion de sweetalert

--- src/app/components/form-contact/form-contact.component.ts
+++ src/app/components/form-contact/form-contact.component.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Component } from '@angular/core';
-import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-form-contact',
@@ -20,26 +19,6 @@ export class FormContactComponent {
 
   close() {
     this.dialogref.close();
-  }
-
-  sendForm() {
-    if (this.formContact.valid) {
-      const formData = new FormData();
-      const { value } = this.formContact;
-      for (const key in value) {
-        formData.append(key, value[key]);
-      }
-      this.htt.post('https://formsubmit.co/jimenezkev1040@gmail.com', formData).subscribe({
-        next: result => {
-          Swal.fire({ icon: "success", title: "Your message has been sent!", showConfirmButton: false, timer: 1500 });
-          setTimeout(() => this.close(), 1000);
-        },
-        error: e => {
-          Swal.fire({ position: 'center', icon: 'error', title: 'An error has occurred!', text: 'Please try again later', confirmButtonColor: 'rgb(220,53,69)' });
-          setTimeout(() => this.close(), 1000);
-        }
-      });
-    }
   }
 
 }
